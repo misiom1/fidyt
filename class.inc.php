8d528874dd8eb6a3d0a9b6ae7ae242afa8b372fe
root <root@adinor-VirtualBox.(none)>
2011-11-16 22:15:51 +0100

    jestembotem

--- class.inc.php
+++ class.inc.php
@@ -1,118 +1,174 @@
-﻿<?
+<?
 require('config.inc.php');
 class Form{
-	public $db;
-	public function kategoria($method, $action)
-	{
-		try
-		{
-			$db = new PDO(BAZA.':host=localhost;dbname='.DB, LOGIN, PASSWORD);
-		}
-		catch (PDOException $e)
-		{
-			print "Błąd poł±czenia z bazą!: " . $e->getMessage() . "<br/>";
-    		die();
-		}
-		$sql = $db->query('SELECT id_kategoria, nazwa FROM kategoria');
-		
-		echo '<form method=\''.$method.'\' action=\''.$action.'\' name=kategoriaForm>';
-		echo '<table>';
-		echo '<tr><td>Id nadkategorii:</td><td>';
-		echo '<select name="idnadkat"><option value="0">0</option>';
-		foreach($sql as $row)
-		{
-			echo '<option value="'.$row['id_kategoria'].'">'.$row['id_kategoria'].' - '.$row['nazwa'].'</option>';
-		}
-		$sql->closeCursor();
-		echo '</select></td></tr>';
-		echo '<tr><td>Nazwa:</td><td><input type=text name=nazwa></td></tr>';
-		echo '<tr><td>Nazwa skrócona:</td><td><input type=text name=nazwaSkrocona></td></tr>';
-		echo '<tr><td>Opis:</td><td><textarea rows=5 cols=40 name=opis></textarea></td></tr>';
-		echo '<tr><td>Usun:</td><td><select name="usun"><option value=0>0</option><option value=1>1</option></select></td></tr>';
-		echo '<tr><td>Ukryj:</td><td><select name="ukryj"><option value=0>0</option><option value=1>1</option></select></td></tr>';
-		echo '<tr><td>Kolejnosc sortowania:</td><td><select name="kolejnosc_sort"><option value=0>0</option><option value=1>1</option></select></td></tr>';
-		echo '<tr><td colspan=2><input type=submit value=Submit></td></tr>';
-		echo '</table>';
-	}
-	public function zadanie($method, $action)
-	{
-		try
-		{
-			$db = new PDO(BAZA.':host=localhost;dbname='.DB, LOGIN, PASSWORD);
-		}
-		catch (PDOException $e)
-		{
-			print "Bł±d poł±czenia z baz±!: " . $e->getMessage() . "<br/>";
-    		die();
-		}
-		$sql = $db->query('SELECT id_kategoria, nazwa FROM kategoria');
-		?>
-		<form name="dodzadanie" method="<?=$method?>" action="<?=$action?>">
-		<table><tr><td>Treśc:</td><td><textarea name="tresc" rows="10" cols="40"></textarea></td></tr>
-		<tr><td>Rozwiązanie:</td><td><textarea name="rozwiazanie" rows="10" cols="40"></textarea></td></tr>
-		<tr><td>Poziom trudności:</td><td><input type="text" name="poz_trudnosci"></td></tr>
-		<tr><td>Kategoria/e</td><td><select name="kat[]" multiple="multiple">
-		<?
-		foreach($sql as $row)
-		{
-			?>
-				<option value="<?=$row['id_kategoria']?>"><?=$row['nazwa']?></option>
-			<?
-		}
-		$sql->closeCursor();
-		?>
-		</select>
-		<tr><td>Ukryj:</td><td><select name="ukryj"><option value=0>0</option><option value=1>1</option></select></td></tr>
-		<tr><td>Usuń:</td><td><select name="usun"><option value=0>0</option><option value=1>1</option></select></td></tr>
-		<tr><td colspan="2"><input type="submit" value="Submit"></td></tr>
-		</form>
-		<?
-	}
+public $db;
+public function kategoria($method, $action)
+{
+try
+{
+$db = new PDO(BAZA.':host=localhost;dbname='.DB, LOGIN, PASSWORD);
+}
+catch (PDOException $e)
+{
+print "Błąd poł±czenia z bazą!: " . $e->getMessage() . "<br/>";
+     die();
+}
+$sql = $db->query('SELECT id_kategoria, nazwa FROM kategoria');
+
+echo '<form method=\''.$method.'\' action=\''.$action.'\' name=kategoriaForm>';
+echo '<table>';
+echo '<tr><td>Id nadkategorii:</td><td>';
+echo '<select name="idnadkat"><option value="0">0</option>';
+foreach($sql as $row)
+{
+echo '<option value="'.$row['id_kategoria'].'">'.$row['id_kategoria'].' - '.$row['nazwa'].'</option>';
+}
+$sql->closeCursor();
+echo '</select></td></tr>';
+echo '<tr><td>Nazwa:</td><td><input type=text name=nazwa></td></tr>';
+echo '<tr><td>Nazwa skrócona:</td><td><input type=text name=nazwaSkrocona></td></tr>';
+echo '<tr><td>Opis:</td><td><textarea rows=5 cols=40 name=opis></textarea></td></tr>';
+echo '<tr><td>Usun:</td><td><select name="usun"><option value=0>0</option><option value=1>1</option></select></td></tr>';
+echo '<tr><td>Ukryj:</td><td><select name="ukryj"><option value=0>0</option><option value=1>1</option></select></td></tr>';
+echo '<tr><td>Kolejnosc sortowania:</td><td><select name="kolejnosc_sort"><option value=0>0</option><option value=1>1</option></select></td></tr>';
+echo '<tr><td colspan=2><input type=submit value=Submit></td></tr>';
+echo '</table>';
+}
+public function zadanie($method, $action)
+{
+try
+{
+$db = new PDO(BAZA.':host=localhost;dbname='.DB, LOGIN, PASSWORD);
+}
+catch (PDOException $e)
+{
+print "Bł±d poł±czenia z baz±!: " . $e->getMessage() . "<br/>";
+     die();
+}
+$sql = $db->query('SELECT id_kategoria, nazwa FROM kategoria');
+?>
+<form name="dodzadanie" method="<?=$method?>" action="<?=$action?>">
+<table><tr><td>Treśc:</td><td><textarea name="tresc" rows="10" cols="40"></textarea></td></tr>
+<tr><td>Rozwiązanie:</td><td><textarea name="rozwiazanie" rows="10" cols="40"></textarea></td></tr>
+<tr><td>Poziom trudności:</td><td><input type="text" name="poz_trudnosci"></td></tr>
+<tr><td>Kategoria/e</td><td><select name="kat[]" multiple="multiple">
+<?
+foreach($sql as $row)
+{
+?>
+<option value="<?=$row['id_kategoria']?>"><?=$row['nazwa']?></option>
+<?
+}
+$sql->closeCursor();
+?>
+</select>
+<tr><td>Ukryj:</td><td><select name="ukryj"><option value=0>0</option><option value=1>1</option></select></td></tr>
+<tr><td>Usuń:</td><td><select name="usun"><option value=0>0</option><option value=1>1</option></select></td></tr>
+<tr><td colspan="2"><input type="submit" value="Submit"></td></tr>
+</form>
+<?
+}
 
 }
 class SQL{
-	public function checkDbEngine($dbname, $user, $password)
-	{
-		if(function_exists('sqlsrv_connect') && sqlsrv_connect('localhost', $user, $password)) return 'mssql';
-		elseif(function_exists('mssql_connect') && mssql_connect('localhost', $user, $password)) return 'mssql';
-		elseif(mysql_connect('localhost', $user, $password)) return 'mysql';
-	}
-	public function kategoria_add($idNadKat, $nazwa, $nazwa_skr, $opis, $usun, $ukryj, $kolejn_sort)
-	{
-		try
-		{
-			$db = new PDO(BAZA.':host=localhost;dbname='.DB, LOGIN, PASSWORD);
-		}
-		catch (PDOException $e)
-		{
-			print "Błąd połączenia z bazą!: " . $e->getMessage() . "<br/>";
-    		die();
-		}
-		$db->exec('INSERT INTO kategoria(id_nadkategoria, nazwa, nazwa_skrocona, opis, usun, ukryj, kolejnosc_sortowania) VALUES(\''.$idNadKat.'\', \''.$nazwa.'\', \''.$nazwa_skr.'\', \''.$opis.'\', \''.$usun.'\', \''.$ukryj.'\', \''.$kolejn_sort.'\')') or die(print_r($db->errorInfo(), true));
-	}
-	public function zadanie_add($tresc, $rozwiazanie, $poz_trudnosci, $kat, $ukryj, $usun)
-	{
-		try
-		{
-			$db = new PDO(BAZA.':host=localhost;dbname='.DB, LOGIN, PASSWORD);
-		}
-		catch (PDOException $e)
-		{
-			print "Błąd połączenia z bazą!: " . $e->getMessage() . "<br/>";
-    		die();
-		}
-		$date = date('Y-m-d');
-		$db->exec('INSERT INTO zadanie(tresc, rozwiazanie, data_dodania, data_modyfikacji, poziom_trudnosci, usun, ukryj) VALUES(\''.$tresc.'\', \''.$rozwiazanie.'\', \''.$date.'\', \''.$date.'\', \''.$poz_trudnosci.'\', \''.$usun.'\', \''.$ukryj.'\')') or die(print_r($db->errorInfo(), true));
-		$sql = $db->query('SELECT id_zadanie FROM zadanie WHERE data_dodania=\''.$date.'\' AND tresc=\''.$tresc.'\'') or die(print_r($db->errorInfo(), true));
-		$row = $sql -> fetch();
+public function checkDbEngine($dbname, $user, $password)
+{
+if(function_exists('sqlsrv_connect') && sqlsrv_connect('localhost', $user, $password)) return 'mssql';
+elseif(function_exists('mssql_connect') && mssql_connect('localhost', $user, $password)) return 'mssql';
+elseif(mysql_connect('localhost', $user, $password)) return 'mysql';
+}
+public function kategoria_add($idNadKat, $nazwa, $nazwa_skr, $opis, $usun, $ukryj, $kolejn_sort)
+{
+try
+{
+$db = new PDO(BAZA.':host=localhost;dbname='.DB, LOGIN, PASSWORD);
+}
+catch (PDOException $e)
+{
+print "Błąd połączenia z bazą!: " . $e->getMessage() . "<br/>";
+     die();
+}
+$db->exec('INSERT INTO kategoria(id_nadkategoria, nazwa, nazwa_skrocona, opis, usun, ukryj, kolejnosc_sortowania) VALUES(\''.$idNadKat.'\', \''.$nazwa.'\', \''.$nazwa_skr.'\', \''.$opis.'\', \''.$usun.'\', \''.$ukryj.'\', \''.$kolejn_sort.'\')') or die(print_r($db->errorInfo(), true));
+}
+public function zadanie_add($tresc, $rozwiazanie, $poz_trudnosci, $kat, $ukryj, $usun)
+{
+try
+{
+$db = new PDO(BAZA.':host=localhost;dbname='.DB, LOGIN, PASSWORD);
+}
+catch (PDOException $e)
+{
+print "Błąd połączenia z bazą!: " . $e->getMessage() . "<br/>";
+     die();
+}
+$date = date('Y-m-d');
+$db->exec('INSERT INTO zadanie(tresc, rozwiazanie, data_dodania, data_modyfikacji, poziom_trudnosci, usun, ukryj) VALUES(\''.$tresc.'\', \''.$rozwiazanie.'\', \''.$date.'\', \''.$date.'\', \''.$poz_trudnosci.'\', \''.$usun.'\', \''.$ukryj.'\')') or die(print_r($db->errorInfo(), true));
+$sql = $db->query('SELECT id_zadanie FROM zadanie WHERE data_dodania=\''.$date.'\' AND tresc=\''.$tresc.'\'') or die(print_r($db->errorInfo(), true));
+$row = $sql -> fetch();
 
-		for($i=0;$i<count($kat);$i++)
-		{
-			$sq = $db->query('SELECT kolejnosc_sortowania FROM kategoria WHERE id_kategoria=\''.$kat[$i].'\'') or die(print_r($db->errorInfo(), true));
-			$w = $sq -> fetch();
-			$db->exec('INSERT INTO zadanie_kategoria VALUES(\''.$kat[$i].'\', \''.$row['id_zadanie'].'\', \''.$w['kolejnosc_sortowania'].'\')') or die(print_r($db->errorInfo(), true));
-		}
-	}
+for($i=0;$i<count($kat);$i++)
+{
+$sq = $db->query('SELECT kolejnosc_sortowania FROM kategoria WHERE id_kategoria=\''.$kat[$i].'\'') or die(print_r($db->errorInfo(), true));
+$w = $sq -> fetch();
+$db->exec('INSERT INTO zadanie_kategoria VALUES(\''.$kat[$i].'\', \''.$row['id_zadanie'].'\', \''.$w['kolejnosc_sortowania'].'\')') or die(print_r($db->errorInfo(), true));
+}
+}
+public function show_all()
+{
+try
+{
+$db = new PDO(BAZA.':host=localhost;dbname='.DB, LOGIN, PASSWORD);
+}
+catch (PDOException $e)
+{
+print "Błąd połączenia z bazą!: " . $e->getMessage() . "<br/>";
+     die();
+}
+$sql = $db->query('SELECT id_kategoria, id_nadkategoria, nazwa, nazwa_skrocona, opis, usun, ukryj, kolejnosc_sortowania FROM kategoria');
+foreach($sql as $row)
+{
+echo '<table>';
+echo '<tr><td>Id kategorii:</td><td>'.$row['id_kategoria'].'</td></tr>';
+echo '<tr><td>Id nadkategorii:</td><td>'.$row['id_nadkategoria'].'</td></tr>';
+echo '<tr><td>Nazwa:</td><td>'.$row['nazwa'].'</td></tr>';
+echo '<tr><td>Nazwa skrócona:</td><td>'.$row['nazwa_skrocona'].'</td></tr>';
+echo '<tr><td>Opis:</td><td><'.$row['opis'].'</td></tr>';
+echo '<tr><td>Usun:</td><td>'.$row['usun'].'</td></tr>';
+echo '<tr><td>Ukryj:</td><td>'.$row['ukryj'].'</td></tr>';
+echo '<tr><td>Kolejnosc sortowania:</td><td><'.$row['kolejnosc_sortowania'].'</td></tr>';
+echo '</table>';
+echo '<br><br>';
+}
+$sql->closeCursor();
+}
+public function show()
+{
+try
+{
+$db = new PDO(BAZA.':host=localhost;dbname='.DB, LOGIN, PASSWORD);
+}
+catch (PDOException $e)
+{
+print "Błąd połączenia z bazą!: " . $e->getMessage() . "<br/>";
+     die();
+}
+$sql = $db->query('SELECT id_kategoria, id_nadkategoria, nazwa, nazwa_skrocona, opis, usun, ukryj, kolejnosc_sortowania FROM kategoria');
+foreach($sql as $row)
+{
+echo '<table>';
+echo '<tr><td>Id kategorii:</td><td>'.$row['id_kategoria'].'</td></tr>';
+echo '<tr><td>Id nadkategorii:</td><td>'.$row['id_nadkategoria'].'</td></tr>';
+echo '<tr><td>Nazwa:</td><td>'.$row['nazwa'].'</td></tr>';
+echo '<tr><td>Nazwa skrócona:</td><td>'.$row['nazwa_skrocona'].'</td></tr>';
+echo '<tr><td>Opis:</td><td><'.$row['opis'].'</td></tr>';
+echo '<tr><td>Usun:</td><td>'.$row['usun'].'</td></tr>';
+echo '<tr><td>Ukryj:</td><td>'.$row['ukryj'].'</td></tr>';
+echo '<tr><td>Kolejnosc sortowania:</td><td><'.$row['kolejnosc_sortowania'].'</td></tr>';
+echo '</table>';
+echo '<br><br>';
+}
+$sql->closeCursor();
+}
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -13,25 +13,30 @@ if(empty($_SERVER['QUERY_STRING']))
 {
 ?>
 <a href="?dodkatform">Dodaj kategorie</a><br>
-<a href="?dodzadform">Dodaj zadanie</a>
+<a href="?dodzadform">Dodaj zadanie</a><br>
+<a href="?showall">Pokaz wszystkie</a>
 <?
 }
 elseif(isset($_GET['dodkatform']))
-{	
-	$form->kategoria('POST', '?dodkat');
+{
+$form->kategoria('POST', '?dodkat');
 }
 elseif(isset($_GET['dodkat']))
 {
-	$sql->kategoria_add($_POST['idnadkat'], $_POST['nazwa'], $_POST['nazwaSkrocona'], $_POST['opis'], $_POST['usun'], $_POST['ukryj'], $_POST['kolejnosc_sort']);
+$sql->kategoria_add($_POST['idnadkat'], $_POST['nazwa'], $_POST['nazwaSkrocona'], $_POST['opis'], $_POST['usun'], $_POST['ukryj'], $_POST['kolejnosc_sort']);
 }
 elseif(isset($_GET['dodzadform']))
 {
-	$form->zadanie('POST', '?dodzad');
+$form->zadanie('POST', '?dodzad');
 }
 elseif(isset($_GET['dodzad']))
 {
-	//var_dump($_POST['kat']);
-	$sql->zadanie_add($_POST['tresc'], $_POST['rozwiazanie'], $_POST['poz_trudnosci'], $_POST['kat'], $_POST['ukryj'], $_POST['usun']);
+//var_dump($_POST['kat']);
+$sql->zadanie_add($_POST['tresc'], $_POST['rozwiazanie'], $_POST['poz_trudnosci'], $_POST['kat'], $_POST['ukryj'], $_POST['usun']);
+}
+elseif(isset($_GET['showall']))
+{
+$sql->show_all();
 }
 ?>
 </body>
