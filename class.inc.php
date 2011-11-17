5cb49cf522ca928db5d7a5345353366f572a2d7c
Misio <misiom1@gmail.com>
2011-11-17 16:40:58 +0100

    php

--- class.inc.php
+++ class.inc.php
@@ -13,55 +13,55 @@ class Form{
             die();
         }
         $sql = $db->query('SELECT id_kategoria, nazwa FROM kategoria');
-if($idkat!=0)
-{
-$s = $db->query('SELECT * FROM kategoria WHERE id_kategoria=\''.$idkat.'\'');
-$up = $s -> fetch();
-}
+        if ($idkat!=0)
+        {
+            $s = $db->query('SELECT * FROM kategoria WHERE id_kategoria=\''.$idkat.'\'');
+            $up = $s -> fetch();
+        }
         echo '<form method=\''.$method.'\' action=\''.$action.'\' name=kategoriaForm>';
         echo '<table>';
         echo '<tr><td>Id nadkategorii:</td><td>';
         echo '<select name="idnadkat"><option value="0"';
-if($idkat!=0 && $up['id_nadkategoria']==0) echo ' selected="selected"';
-echo '>0</option>';
+        if ($idkat!=0 && $up['id_nadkategoria']==0) echo ' selected="selected"';
+        echo '>0</option>';
         foreach($sql as $row)
         {
-            if($idkat!=0 && $up['id_kategoria']!=$row['id_kategoria'])
-{
-echo '<option value="'.$row['id_kategoria'].'"';
-if($idkat!=0 && $up['id_nadkategoria']==$row['id_kategoria']) echo ' selected="selected"';
-echo '>'.$row['id_kategoria'].' - '.$row['nazwa'].'</option>';
-}
+            if ($idkat!=0 && $up['id_kategoria']!=$row['id_kategoria'])
+            {
+                echo '<option value="'.$row['id_kategoria'].'"';
+                if ($idkat!=0 && $up['id_nadkategoria']==$row['id_kategoria']) echo ' selected="selected"';
+                echo '>'.$row['id_kategoria'].' - '.$row['nazwa'].'</option>';
+            }
         }
         echo '</select></td></tr>';
         echo '<tr><td>Nazwa:</td><td><input type=text name=nazwa';
-if($idkat!=0) echo ' value="'.$up['nazwa'].'"';
-echo'></td></tr>';
+        if ($idkat!=0) echo ' value="'.$up['nazwa'].'"';
+        echo'></td></tr>';
         echo '<tr><td>Nazwa skrócona:</td><td><input type=text name=nazwaSkrocona';
-if($idkat!=0) echo ' value="'.$up['nazwa_skrocona'].'"';
-echo '></td></tr>';
+        if ($idkat!=0) echo ' value="'.$up['nazwa_skrocona'].'"';
+        echo '></td></tr>';
         echo '<tr><td>Opis:</td><td><textarea rows=5 cols=40 name=opis>';
-if($idkat!=0) echo $up['opis'];
-echo '</textarea></td></tr>';
+        if ($idkat!=0) echo $up['opis'];
+        echo '</textarea></td></tr>';
         echo '<tr><td>Usun:</td><td><select name="usun"><option value="0"';
-if($idkat!=0 && $up['usun']==0) echo ' selected="selected"';
-echo '>0</option><option value="1"';
-if($idkat!=0 && $up['usun']==1) echo ' selected="selected"';
-echo '>1</option></select></td></tr>';
+        if ($idkat!=0 && $up['usun']==0) echo ' selected="selected"';
+        echo '>0</option><option value="1"';
+        if ($idkat!=0 && $up['usun']==1) echo ' selected="selected"';
+        echo '>1</option></select></td></tr>';
         echo '<tr><td>Ukryj:</td><td><select name="ukryj"><option value="0"';
-if($idkat!=0 && $up['ukryj']==0) echo ' selected="selected"';
-echo '>0</option><option value="1"';
-if($idkat!=0 && $up['ukryj']==1) echo ' selected="selected"';
-echo '>1</option></select></td></tr>';
+        if ($idkat!=0 && $up['ukryj']==0) echo ' selected="selected"';
+        echo '>0</option><option value="1"';
+        if ($idkat!=0 && $up['ukryj']==1) echo ' selected="selected"';
+        echo '>1</option></select></td></tr>';
         echo '<tr><td>Kolejność sortowania:</td><td><select name="kolejnosc_sort"><option value="0"';
-if($idkat!=0 && $up['kolejnosc_sortowania']==0) echo ' selected="selected"';
-echo '>0</option><option value="1"';
-if($idkat!=0 && $up['kolejnosc_sortowania']==1) echo ' selected="selected"';
-echo '>1</option></select></td></tr>';
+        if ($idkat!=0 && $up['kolejnosc_sortowania']==0) echo ' selected="selected"';
+        echo '>0</option><option value="1"';
+        if ($idkat!=0 && $up['kolejnosc_sortowania']==1) echo ' selected="selected"';
+        echo '>1</option></select></td></tr>';
         echo '<tr><td colspan=2><input type=submit value=Submit></td></tr>';
-if($idkat!=0) echo '<input type="hidden" name="idkat" value="'.$idkat.'">';
+        if ($idkat!=0) echo '<input type="hidden" name="idkat" value="'.$idkat.'">';
         echo '</table></form>';
-$sql->closeCursor();
+        $sql->closeCursor();
     }
     public function zadanie($method, $action, $zadid="0")
     {
@@ -75,55 +75,55 @@ $sql->closeCursor();
             die();
         }
         $sql = $db->query('SELECT id_kategoria, nazwa FROM kategoria');
-if($zadid!=0)
-{
-$s = $db->query('SELECT * FROM zadanie WHERE id_zadanie=\''.$zadid.'\'');
-$up = $s->fetch();
-$kat = $db->query('SELECT * FROM zadanie_kategoria WHERE id_zadanie=\''.$zadid.'\'');
-$array = $kat->fetchAll(PDO::FETCH_ASSOC);
-}
-        ?>
-<form name="ZadForm" method="<?=$method?>" action="<?=$action?>">
-<table><tr><td>Treśc:</td><td><textarea name="tresc" rows="10" cols="40">
-<? if($zadid!=0) echo trim($up['tresc']); ?>
-</textarea></td></tr>
-<tr><td>Rozwiązanie:</td><td><textarea name="rozwiazanie" rows="10" cols="40">
-<? if($zadid!=0) echo trim($up['rozwiazanie']); ?>
-</textarea></td></tr>
-<tr><td>Poziom trudności:</td><td><input type="text" name="poz_trudnosci"
-<? if($zadid!=0) echo 'value="'.$up['poziom_trudnosci'].'"'; ?>
-></td></tr>
-<tr><td>Kategoria/e</td><td><select name="kat[]" multiple="multiple">
-<?
-        foreach($sql as $row)
+        if ($zadid!=0)
         {
-$selected=0;
-if($zadid!=0)
-{
-foreach($array as $x)
-{
-if($row['id_kategoria']==$x['id_kategoria']){ $selected=1;break; }
-}
-}
-?>
-<option value="<?=$row['id_kategoria']?>"<? if($zadid!=0 && $selected==1) echo ' selected="selected"'; ?>><?=$row['nazwa']?></option>
-<?
+            $s = $db->query('SELECT * FROM zadanie WHERE id_zadanie=\''.$zadid.'\'');
+            $up = $s->fetch();
+            $kat = $db->query('SELECT * FROM zadanie_kategoria WHERE id_zadanie=\''.$zadid.'\'');
+            $array = $kat->fetchAll(PDO::FETCH_ASSOC);
         }
-        
         ?>
-</select>
-<tr><td>Ukryj:</td><td><select name="ukryj"><option value="0"<?if($zadid!=0 && $up['ukryj']==0) echo ' selected="selected"'; ?>>0</option><option value="1"<?if($zadid!=0 && $up['ukryj']==1) echo ' selected="selected"'; ?>>1</option></select></td></tr>
-<tr><td>Usuń:</td><td><select name="usun"><option value="0"<?if($zadid!=0 && $up['usun']==0) echo ' selected="selected"'; ?>>0</option><option value="1"<?if($zadid!=0 && $up['usun']==1) echo ' selected="selected"'; ?>>1</option></select></td></tr>
-<tr><td colspan="2"><input type="submit" value="Submit"></td></tr></table>
-<? if($zadid!=0) echo '<input type="hidden" name="zadid" value="'.$zadid.'">'; ?>
-</form>
-<?
-$sql->closeCursor();
-if($zadid!=0)
-{
-$kat->closeCursor();
-$s->closeCursor();
-}
+        <form name="ZadForm" method="<?=$method?>" action="<?=$action?>">
+                                    <table><tr><td>Treśc:</td><td><textarea name="tresc" rows="10" cols="40">
+                                            <? if ($zadid!=0) echo trim($up['tresc']);        ?>
+        </textarea></td></tr>
+        <tr><td>Rozwiązanie:</td><td><textarea name="rozwiazanie" rows="10" cols="40">
+                <? if ($zadid!=0) echo trim($up['rozwiazanie']);        ?>
+        </textarea></td></tr>
+        <tr><td>Poziom trudności:</td><td><input type="text" name="poz_trudnosci"
+                <? if ($zadid!=0) echo 'value="'.$up['poziom_trudnosci'].'"';        ?>
+        ></td></tr>
+        <tr><td>Kategoria/e</td><td><select name="kat[]" multiple="multiple">
+                <?
+                foreach($sql as $row)
+        {
+            $selected=0;
+            if ($zadid!=0)
+            {
+                foreach($array as $x)
+                {
+                    if ($row['id_kategoria']==$x['id_kategoria'])                    {                        $selected=1;                        break;                    }
+                }
+            }
+            ?>
+            <option value="<?=$row['id_kategoria']?>"<? if ($zadid!=0 && $selected==1) echo ' selected="selected"';            ?>><?=$row['nazwa']?></option>
+                  <?
+              }
+
+              ?>
+              </select>
+              <tr><td>Ukryj:</td><td><select name="ukryj"><option value="0"<?if ($zadid!=0 && $up['ukryj']==0) echo ' selected="selected"';        ?>>0</option><option value="1"<?if ($zadid!=0 && $up['ukryj']==1) echo ' selected="selected"';        ?>>1</option></select></td></tr>
+        <tr><td>Usuń:</td><td><select name="usun"><option value="0"<?if ($zadid!=0 && $up['usun']==0) echo ' selected="selected"';        ?>>0</option><option value="1"<?if ($zadid!=0 && $up['usun']==1) echo ' selected="selected"';        ?>>1</option></select></td></tr>
+        <tr><td colspan="2"><input type="submit" value="Submit"></td></tr></table>
+                                        <? if ($zadid!=0) echo '<input type="hidden" name="zadid" value="'.$zadid.'">';        ?>
+        </form>
+        <?
+        $sql->closeCursor();
+        if ($zadid!=0)
+        {
+            $kat->closeCursor();
+            $s->closeCursor();
+        }
     }
 
 }
@@ -252,8 +252,8 @@ class SQL{
 
         $sql->closeCursor();
     }
-public function editkat($idkat, $idNadKat, $nazwa, $nazwa_skr, $opis, $usun, $ukryj, $kolejn_sort)
-{
+    public function editkat($idkat, $idNadKat, $nazwa, $nazwa_skr, $opis, $usun, $ukryj, $kolejn_sort)
+    {
         try
         {
             $db = new PDO(BAZA.':host=localhost;dbname='.DB, LOGIN, PASSWORD);
@@ -263,10 +263,10 @@ public function editkat($idkat, $idNadKat, $nazwa, $nazwa_skr, $opis, $usun, $uk
             print "Błąd połączenia z bazą!: " . $e->getMessage() . "<br/>";
             die();
         }
-$db->exec('UPDATE kategoria SET id_nadkategoria=\''.$idNadKat.'\', nazwa=\''.$nazwa.'\', nazwa_skrocona=\''.$nazwa_skr.'\', opis=\''.$opis.'\', usun=\''.$usun.'\', ukryj=\''.$ukryj.'\', kolejnosc_sortowania=\''.$kolejn_sort.'\' WHERE id_kategoria=\''.$idkat.'\'') or die(print_r($db->errorInfo(), true));
-}
-public function editzad($zadid, $tresc, $rozwiazanie, $poz_trudnosci, $kat, $ukryj, $usun)
-{
+        $db->exec('UPDATE kategoria SET id_nadkategoria=\''.$idNadKat.'\', nazwa=\''.$nazwa.'\', nazwa_skrocona=\''.$nazwa_skr.'\', opis=\''.$opis.'\', usun=\''.$usun.'\', ukryj=\''.$ukryj.'\', kolejnosc_sortowania=\''.$kolejn_sort.'\' WHERE id_kategoria=\''.$idkat.'\'') or die(print_r($db->errorInfo(), true));
+    }
+    public function editzad($zadid, $tresc, $rozwiazanie, $poz_trudnosci, $kat, $ukryj, $usun)
+    {
         try
         {
             $db = new PDO(BAZA.':host=localhost;dbname='.DB, LOGIN, PASSWORD);
@@ -276,41 +276,42 @@ public function editzad($zadid, $tresc, $rozwiazanie, $poz_trudnosci, $kat, $ukr
             print "Błąd połączenia z bazą!: " . $e->getMessage() . "<br/>";
             die();
         }
-$kitkat = $db->query('SELECT id_kategoria FROM zadanie_kategoria WHERE id_zadanie=\''.$zadid.'\'');
-$array = $kitkat->fetchAll(PDO::FETCH_ASSOC);
-for($i=0;$i<count($kat);$i++)
-{
-$add=0;
-$licz=0;
-foreach($array as $x)
-{
-if($x['id_kategoria']==$kat[$i]){ $add=0; $licz++; break; }
-$add=1;
-$licz++;
-}
-if($licz==0) $add=1;
-if($add==1)
-{
-$sq = $db->query('SELECT kolejnosc_sortowania FROM kategoria WHERE id_kategoria=\''.$kat[$i].'\'') or die(print_r($db->errorInfo(), true));
-$w = $sq -> fetch();
-$db->exec('INSERT INTO zadanie_kategoria VALUES(\''.$kat[$i].'\', \''.$zadid.'\', \''.$w['kolejnosc_sortowania'].'\')') or die(print_r($db->errorInfo(), true));
-}
-}
-foreach($array as $x)
-{
-$del=0;
-for($i=0;$i<count($kat);$i++)
-{
-if($kat[$i]==$x['id_kategoria']){ $del=0; break; }
-$del=1;
-}
-if($del==1)
-{
-$db->exec('DELETE FROM zadanie_kategoria WHERE id_kategoria=\''.$x['id_kategoria'].'\' AND id_zadanie=\''.$zadid.'\'') or die(print_r($db->errorInfo(), true));
-}
-}
-$date = date('Y-m-d');
-$db->query('UPDATE zadanie SET tresc=\''.$tresc.'\', rozwiazanie=\''.$rozwiazanie.'\', data_modyfikacji=\''.$date.'\', poziom_trudnosci=\''.$poz_trudnosci.'\', usun=\''.$usun.'\', ukryj=\''.$ukryj.'\' WHERE id_zadanie=\''.$zadid.'\'') or die(print_r($db->errorInfo(), true));
-}
+        $kitkat = $db->query('SELECT id_kategoria FROM zadanie_kategoria WHERE id_zadanie=\''.$zadid.'\'');
+        $array = $kitkat->fetchAll(PDO::FETCH_ASSOC);
+        for ($i=0;$i<count($kat);$i++)
+        {
+            $add=0;
+            $licz=0;
+            foreach($array as $x)
+            {
+                if ($x['id_kategoria']==$kat[$i])                {                    $add=0;                    $licz++;                    break;                }
+                $add=1;
+                $licz++;
+            }
+            if ($licz==0) $add=1;
+            if ($add==1)
+            {
+                $sq = $db->query('SELECT kolejnosc_sortowania FROM kategoria WHERE id_kategoria=\''.$kat[$i].'\'') or die(print_r($db->errorInfo(), true));
+                $w = $sq -> fetch();
+                $db->exec('INSERT INTO zadanie_kategoria VALUES(\''.$kat[$i].'\', \''.$zadid.'\', \''.$w['kolejnosc_sortowania'].'\')') or die(print_r($db->errorInfo(), true));
+            }
+        }
+        foreach($array as $x)
+        {
+            $del=0;
+            for ($i=0;$i<count($kat);$i++)
+            {
+                if ($kat[$i]==$x['id_kategoria'])                {                    $del=0;                    break;                }
+                $del=1;
+            }
+            if ($del==1)
+            {
+                $db->exec('DELETE FROM zadanie_kategoria WHERE id_kategoria=\''.$x['id_kategoria'].'\' AND id_zadanie=\''.$zadid.'\'') or die(print_r($db->errorInfo(), true));
+            }
+        }
+        $date = date('Y-m-d');
+        $db->query('UPDATE zadanie SET tresc=\''.$tresc.'\', rozwiazanie=\''.$rozwiazanie.'\', data_modyfikacji=\''.$date.'\', poziom_trudnosci=\''.$poz_trudnosci.'\', usun=\''.$usun.'\', ukryj=\''.$ukryj.'\' WHERE id_zadanie=\''.$zadid.'\'') or die(print_r($db->errorInfo(), true));
+    }
 }
 ?>
+
